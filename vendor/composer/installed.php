264fc0fe935f2d5cafff71d576da8b284ebf8941
org100h <hmymerao@procoders.tech>
2024-05-01 14:22:13 +0300

    Add composer autoloader and related classes
    The commit introduces the composer autoloader, classloader and related dependencies. This change enables better management and loading of project's PHP dependencies via Composer.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'procoders/omni',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f4a58b29ee93f84b4e833f8f44fda83b7db3a65c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'procoders/omni' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f4a58b29ee93f84b4e833f8f44fda83b7db3a65c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
